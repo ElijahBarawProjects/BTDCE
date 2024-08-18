0ac7a04c79f0332bdfe332175cd96eea291f6032
Everyday Code <59743315+EverydayCodeNet@users.noreply.github.com>
2024-08-17 21:15:28 -0400

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,23 @@
+ /*
+ *--------------------------------------
+ * Program Name: BTD
+ * Author: Everyday Code
+ * License:
+ * Description: "BTD remake for the TI-84 Plus CE."
+ *--------------------------------------
+*/
+
+#include <math.h>
+#include <tice.h>
+#include <keypadc.h>
+#include <graphx.h>
+#include <fileioc.h>
+
+// converted graphics files
+#include "gfx/gfx.h"
+
+void main(void) {
+    
+    
+    return;
+}
