dc42e2de4fa849f18a4e5586bed632fbe71e00ee
elijah-bae-raw <100040127+elijah-bae-raw@users.noreply.github.com>
2024-08-20 13:37:18 -0400

    Added building a path from a series of points, and path drawer ----------- Co-authored-by: Everyday Code <EverydayCodeNet@users.noreply.github.com>

--- src/main.c
+++ src/main.c
@@ -39,13 +39,17 @@ position_t default_path[] = {
     {180, 0}
 };
 
-void drawPath(game_t *game_state) {
-    return;
+void* safe_malloc(size_t size, int line){
+    void* res = malloc(size);
+    if (res == NULL){
+        dbg_printf("ERROR: call to safe_malloc FAILED on line %d\n", line);
+    }
+    return res;
 }
 
 // in the future, add type of monkey being initialized
 tower_t* initTower(game_t *game) {
-    tower_t *tower = malloc(sizeof(tower_t));
+    tower_t *tower = safe_malloc(sizeof(tower_t), __LINE__);
     tower->attack_speed = 1;
     tower->position = game->cursor;
     tower->target = FIRST;
@@ -86,12 +90,12 @@ void handleKeys(game_t* game) {
                 tower->next = tmp_tower;
             }
             game->cursor_type = NONE;
-
-
-        } else {
-            game->cursor_type = SELECTED;
-        }
+        } 
     } 
+
+    if (kb_Data[6] & kb_Add) {
+        game->cursor_type = SELECTED;
+    }
 
     if (kb_Data[1] & kb_Mode) game->SHOW_STATS = !game->SHOW_STATS;
 
@@ -104,13 +108,17 @@ void drawCursor(game_t* game) {
     gfx_TransparentSprite(dart1, x, y);
     gfx_SetColor(255);
     // replace with selected tower radius
-    gfx_Circle(x, y, 100);
+    // center it based on the tower width/height
+    gfx_Circle(x, y, 50);
 }
 
 void drawMap(game_t* game) {
     gfx_SetColor(158);
     // x, y, width, height
     gfx_FillRectangle(0,0,320,240);
+}
+
+void drawStats(game_t *game) {
     gfx_PrintStringXY("Hearts: ",10, 10);
     gfx_PrintInt(game->hearts, 1);
     gfx_PrintStringXY("Round: ", 10 + 10 + gfx_GetStringWidth("Hearts: 100"), 10);
@@ -138,7 +146,6 @@ void drawBloons(game_t *game) {
 }
 
 void drawProjectiles(game_t *game) {
-
 } 
 
 void drawExitScreen(game_t *game) {
@@ -146,6 +153,38 @@ void drawExitScreen(game_t *game) {
     // count bloons
     
     // draw button
+}
+
+round_t *initRounds(void) {
+    const int NUM_ROUNDS = 100;
+    round_t* rounds = safe_malloc(sizeof(round_t) * NUM_ROUNDS, __LINE__);
+
+    size_t total_num_bloons = 0;
+
+    const int ROUND_ONE_BLOONS = 1;
+    for (int i = 0; i < NUM_ROUNDS; i++) {
+        if (i == 0) {
+            rounds[i].num_bloons = ROUND_ONE_BLOONS;
+        } else {
+            rounds[i].num_bloons = (int) (1.25 * rounds[i - 1].num_bloons);
+        }
+
+        rounds[i].delay = 10;
+        rounds[i].tick = 0;
+        rounds[i].bloons = safe_malloc(sizeof(bloon_t) * rounds[i].num_bloons, __LINE__);
+
+        if (rounds[i].bloons == NULL) {
+            dbg_printf("MALLOCED %lu BALOONS FUCK", total_num_bloons);
+        }
+
+        total_num_bloons += rounds[i].num_bloons;
+
+        for (int j = 0; j < rounds[i].num_bloons; j++) {
+            rounds[i].bloons[j].sprite = base;
+        }
+    }
+
+    return rounds;
 }
 
 int16_t distance(position_t p1, position_t p2) {
@@ -164,6 +203,148 @@ int16_t distance(position_t p1, position_t p2) {
 
 
 
+
+rectangle_t* rectangleFromLineSeg(position_t p1, position_t p2, int16_t width) {
+    // for points defining returned rectangle
+    rectangle_t* rect = safe_malloc(sizeof(rectangle_t), __LINE__);
+    position_t* upper_left =  &(rect->upper_left);
+    position_t* upper_right =  &(rect->upper_right); 
+    position_t* lower_left = &(rect->lower_left);
+    position_t* lower_right = &(rect->lower_right);
+
+    position_t tmp;
+
+    // horiz line seg
+    if (p1.x == p2.x) {
+        rect->kind = HORZ;
+        // make p1 upper, p2 lower
+        if (p1.y > p2.y) {
+            tmp.x = p1.x;
+            tmp.y = p1.y;
+
+            p1.x = p2.x;
+            p1.y = p2.y;
+
+            p2.x = tmp.x;
+            p2.y = tmp.y;
+
+            if (!(p1.y < p2.y)) dbg_printf("ERROR: p1, p2 not swapped successfully! %d\n", __LINE__);
+        }
+        // p1.y < p2.y => p1 is closer to top of canvas
+
+        upper_left->x = p1.x - (width / 2);
+        upper_left->y = p1.y;
+
+        upper_right->x = p1.x + (width / 2);
+        upper_right->y = p1.y;
+
+        lower_left->x = p2.x - (width / 2);
+        lower_left->y = p2.y;
+
+        lower_right->x = p2.x + (width / 2);
+        lower_right->y = p2.y;
+    }
+    // vert line seg
+    else if (p1.y == p2.y) {
+        rect->kind = VERT;
+        // make p1 left, p2 right
+        if (p1.x > p2.x) {
+            tmp.x = p1.x;
+            tmp.y = p1.y;
+
+            p1.x = p2.x;
+            p1.y = p2.y;
+
+            p2.x = tmp.x;
+            p2.y = tmp.y;
+
+            if (!(p1.x < p2.x)) dbg_printf("ERROR: p1, p2 not swapped successfully! %d\n", __LINE__);
+        }
+        // p1.x < p2.x => p1 is left
+
+        upper_left->y = p1.y - (width / 2);
+        upper_left->x = p1.x;
+
+        lower_left->y = p1.y + (width / 2);
+        lower_left->x = p1.x;
+
+        upper_right->y = p2.y - (width / 2);
+        upper_right->x = p2.x;
+
+        lower_right->y = p2.y + (width / 2);
+        lower_right->x = p2.x;
+    }
+    else {
+        rect->kind=DIAG;
+        // make p1 left, p2 right
+        if (p1.x > p2.x) {
+            tmp.x = p1.x;
+            tmp.y = p1.y;
+
+            p1.x = p2.x;
+            p1.y = p2.y;
+
+            p2.x = tmp.x;
+            p2.y = tmp.y;
+        }
+        // p1.x < p2.x => p1 is more left
+
+        // written by chat gpt, not yet checked
+        // Calculate slope of the side
+        double m = (double)(p1.x - p2.x) / (p2.y - p1.y);
+
+        // Calculate displacements along axes
+        double dx = (width / sqrt(1 + (m * m))) * 0.5;
+        double dy = m * dx;
+
+        // Rounding the results to get integer coordinates
+        if (dy > 0) {
+            // adding dy makes it the lower point
+            // subing dy makes it the higher point
+            
+            // left point smaller y
+            upper_left->x = round(p1.x - dx);
+            upper_left->y = round(p1.y - dy);
+
+            // left point larger y
+            lower_left->x = round(p1.x + dx);
+            lower_left->y = round(p1.y + dy);
+
+            // right point smaller y
+            upper_right->x = round(p2.x - dx);
+            upper_right->y = round(p2.y - dy);
+
+            // right point larger y
+            lower_right->x = round(p2.x + dx);
+            lower_right->y = round(p2.y + dy);
+
+        } else { // dy < 0
+            // adding dy makes it the higher point
+            // subing dy makes it the lower point
+
+            // left point larger y
+            lower_left->x = round(p1.x - dx);
+            lower_left->y = round(p1.y - dy);
+
+            // left point smaller y
+            upper_left->x = round(p1.x + dx);
+            upper_left->y = round(p1.y + dy);
+
+            // right point smaller y
+            upper_right->x = round(p2.x + dx);
+            upper_right->y = round(p2.y + dy);
+
+            // right point larger y
+            lower_right->x = round(p2.x - dx);
+            lower_right->y = round(p2.y - dy);
+        }
+    }
+
+
+    return rect;
+}
+
+
 int pathLength(path_t* path){
     int len = 0;
     // loop over line segments
@@ -174,21 +355,116 @@ int pathLength(path_t* path){
     return len;
 }
 
-path_t* newPath(position_t* points, size_t num_points) {
+path_t* newPath(position_t* points, size_t num_points, int16_t width) {
     if (points == NULL || num_points == 0) {
         // use default path
         points = &default_path;
         num_points =sizeof(default_path) / sizeof(position_t);
     }
-    path_t* path = malloc(sizeof(path_t));
+    path_t* path = safe_malloc(sizeof(path_t), __LINE__);
+    path->width = width;
     path->num_points = num_points;
     path->points = points;
     path->length = pathLength(path);
-    dbg_printf("The path has total length %d", path->length);
+
+    // get rectangles from points
+    size_t num_rectangles = num_points - 1;
+    path->rectangles = safe_malloc(sizeof(rectangle_t) * num_rectangles, __LINE__);
+    for (size_t i = 0; i < num_rectangles; i++)
+        (path->rectangles)[i] = *rectangleFromLineSeg(path->points[i], path->points[i+1], width);
+
+
+    #ifdef DEBUG
+    dbg_printf("Rectangles on path:\n");
+    for (size_t i = 0; i < num_rectangles; i++) {
+        rectangle_t r = (path->rectangles)[i];
+        dbg_printf(
+            "upper_left: (%d, %d) \t upper_right: (%d, %d)\n"
+            "lower_left: (%d, %d) \t lower_right: (%d, %d)\n\n",
+            r.upper_left.x, r.upper_left.y, r.upper_right.x, r.upper_right.y,
+            r.lower_left.x, r.lower_left.y, r.lower_right.x, r.lower_right.y);
+    }
+    dbg_printf("... Done! (rect on path)\n");
+
+    #endif
+
+    
+    
+
+    dbg_printf("The path has total length %d\n", path->length);
     return path;
 }
 
+void draw_rectangle(rectangle_t* rect) {
+    switch (rect->kind)
+    {
+    case HORZ:
+    case VERT:
+        dbg_printf(
+            "Filling HORZ/VERT rectangle at at (%d, %d) with width %d and height %d\n",
+            rect->upper_left.x,
+            rect->upper_left.y,
+            rect->upper_right.x -  rect->upper_left.x,
+            rect->lower_left.y - rect->upper_left.y);
 
+        gfx_FillRectangle(
+            rect->upper_left.x,
+            rect->upper_left.y,
+            rect->upper_right.x -  rect->upper_left.x,
+            rect->lower_left.y - rect->upper_left.y);
+        break;
+
+    case DIAG:
+        // cut rectangle down center & draw two triangles
+        gfx_FillTriangle(
+            rect->upper_left.x,
+            rect->upper_left.y,
+            rect->lower_left.x,
+            rect->lower_left.y,
+            rect->upper_right.x,
+            rect->upper_right.y);
+
+        gfx_FillTriangle(
+            rect->upper_right.x,
+            rect->upper_right.y,
+            rect->lower_right.x,
+            rect->lower_right.y,
+            rect->lower_left.x,
+            rect->lower_left.y);
+        break;
+    
+    default:
+        dbg_printf("ERROR: bad rectangle kind\n");
+        break;
+    }
+}
+
+void drawGamePath(game_t* game){
+    path_t* path = game->path;
+    dbg_printf("Drawing path...\n");
+    gfx_SetColor(159);
+    size_t numSegments = path->num_points - 1;
+    position_t segStart;
+    position_t segEnd;
+    for (size_t i = 0; i < numSegments; i ++) {
+        segStart = path->points[i];
+        segEnd = path->points[i+1];
+
+        // draw circle at start of line segment
+        gfx_FillCircle(segStart.x, segStart.y, path->width / 2);
+        
+        // draw segment
+        draw_rectangle(&(path->rectangles[i]));
+
+    }
+
+    // draw end circle
+    if (numSegments > 0) gfx_FillCircle(segEnd.x, segEnd.y, path->width / 2);
+    dbg_printf("... Done drawing path\n");
+}
+
+
+#define DEFAULT_PATH_WIDTH 20 // path width in pixels
 game_t* newGame(position_t* points, size_t num_points){
     /**
      *  typedef struct  {
@@ -207,8 +483,8 @@ game_t* newGame(position_t* points, size_t num_points){
 } game_t;
      */
 
-    game_t* game = malloc(sizeof(game_t));
-    game->path = newPath(points, num_points);
+    game_t* game = safe_malloc(sizeof(game_t), __LINE__);
+    game->path = newPath(points, num_points, DEFAULT_PATH_WIDTH);
     // choose hearts based on game difficulty, to be added
     game->hearts = 100;
     game->coins = 500;
@@ -225,10 +501,14 @@ game_t* newGame(position_t* points, size_t num_points){
 
 
     game->round = 1;
+    dbg_printf("Called init rounds!\n");
+
+    game->rounds = initRounds();
 
     game->AUTOPLAY = false;
     game->SHOW_STATS = false;
     
+    dbg_printf("Returned new game_t\n");
 
     return game;
 }
@@ -238,7 +518,13 @@ void handleGame(game_t* game) {
 
     if (game->hearts <= 0) game->exit = true;
 
+    if (game->rounds[game->round].bloons == game->rounds[game->round].num_bloons) {
+        game->round++;   
+    }
     
+    if (game->rounds == 101) game->exit = true;
+
+
 }
 
 
@@ -276,11 +562,11 @@ void exitGame(game_t* game) {
 void runGame(void) {
     game_t *game = newGame(NULL, 0); // use default path
 
-    
-
     // ugly code for clarity
     while (game->exit == false) {
         drawMap(game);
+        drawGamePath(game);
+        drawStats(game);
         drawTowers(game);
         drawBloons(game);
         drawProjectiles(game);
--- src/structs.h
+++ src/structs.h
@@ -19,21 +19,33 @@ typedef struct {
     int16_t y;
 } position_t;
 
+typedef struct {
+    // four points
+    position_t upper_left;
+    position_t lower_left;
+    position_t upper_right;
+    position_t lower_right;
+
+    enum {HORZ, VERT, DIAG} kind;
+
+} rectangle_t;
 
 typedef struct {
     position_t* points; // the points which make up the piecewise path
+    rectangle_t* rectangles;
     size_t num_points; // length of points
     int length; // sum of lengths of line segments
-
+    int width; // width of the path
 } path_t;
 
 typedef struct bloon_t {
    position_t position;
    struct bloon_t *next;
+   gfx_sprite_t *sprite;
    int speed;
 } bloon_t;
 
-typedef struct tower_t {
+typedef struct  {
     struct tower_t *next;
     position_t position;
     int attack_speed;
@@ -41,7 +53,7 @@ typedef struct tower_t {
     int radius;
 } tower_t;
 
-typedef struct projectile_t {
+typedef struct {
     struct projectile_t *next;
     position_t position;
     int speed;
@@ -49,16 +61,25 @@ typedef struct projectile_t {
 } projectile_t;
 
 typedef struct  {
+    int num_bloons;
+    int delay;
+    int tick;
+    bloon_t *bloons;
+} round_t;
+
+typedef struct {
     path_t* path;
     int hearts;
     int coins;
     tower_t* towers;
     bloon_t* bloons;
     projectile_t* projectiles;
+    round_t* rounds;
     bool exit;
     enum {SELECTED, INFO, NONE} cursor_type;
     position_t cursor;
     int round;
+
     bool AUTOPLAY;
     bool SHOW_STATS;
 } game_t;
